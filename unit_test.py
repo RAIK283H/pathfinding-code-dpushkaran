d41d2b3a6edb088b5f8ac0281edb1be9de642bbe
dpushkaran <91815543+dpushkaran@users.noreply.github.com>
2024-10-06 22:43:16 -0500

    Unit test added

--- unit_test.py
+++ unit_test.py
@@ -1,6 +1,10 @@
 import math
 import unittest
 
+# new imports for my tests
+import global_game_data
+import pathing
+import graph_data
 
 class TestPathFinding(unittest.TestCase):
 
@@ -23,6 +27,18 @@ class TestPathFinding(unittest.TestCase):
         self.assertNotEqual(almost_pi, pi)
         self.assertAlmostEqual(first=almost_pi, second=pi, delta=1e-1)
 
+    #Checks if the start and end node is initalized correctly for the first graph
+    def test_path_contains_start_and_end(self):
+        global_game_data.current_graph_index = 0
+        global_game_data.target_node = [2] 
+
+        start_node_index = 0
+        end_node_index = len(graph_data.graph_data[global_game_data.current_graph_index]) - 1
+        path = pathing.get_random_path()
+
+        # Check that the first node is the start node and the last node is the end node
+        self.assertEqual(path[0], start_node_index, "The path should start at the start node.")
+        self.assertEqual(path[-1], end_node_index, "The path should end at the end node.")
 
 if __name__ == '__main__':
     unittest.main()
